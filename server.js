939c3b93f28b035f132b183c1f68ce479e261c15
LaneBrantley <122322578+LaneBrantley@users.noreply.github.com>
2023-09-16 17:18:49 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
@@ -25,8 +26,23 @@ app.get('/hello', (req, res) => {
     res.send('Hello world!');
 });
 
-app.get('/budget', (req, res) =>{
-    res.json(budget);
+app.get('/budget', (req, res) => {
+    fs.readFile('budget.json', 'utf8', (err, data) => { //Using fs module from nodejs to read from budget.json
+        if (err) { //In case of error, this will be thrown
+            console.error('Error reading budget.json:', err);
+            res.status(500).json({ error: 'Unable to read budget data' });
+            return;
+        }
+
+        //Parses json file and puts it inside of budgetData
+        try {
+            const budgetData = JSON.parse(data);
+            res.json(budgetData); //Responds with budgetData array
+        } catch (parseError) { //In case of a parsing error
+            console.error('Error parsing JSON:', parseError);
+            res.status(500).json({ error: 'Unable to parse budget data' });
+        }
+    });
 });
 
 app.listen(port, () => {
